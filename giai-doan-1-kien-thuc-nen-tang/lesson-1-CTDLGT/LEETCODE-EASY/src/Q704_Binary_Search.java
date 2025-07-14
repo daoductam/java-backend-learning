77ef4653258d8748cc8101ac20b6069396b42d33
Dao Duc Tam <ddt@gmail.com>
2025-07-15 01:45:58 +0700

    feat: Luyên leetcode 2 bài

---
+++ giai-doan-1-kien-thuc-nen-tang/lesson-1-CTDLGT/LEETCODE-EASY/src/Q704_Binary_Search.java
@@ -0,0 +1,38 @@
+public class Q704_Binary_Search {
+    /*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+ If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    Ý tưởng: Tìm kiếm nhị phân
+     */
+    public static int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+
+        while (l <= r) {
+            int mid =l+ (r-l)/2;
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
