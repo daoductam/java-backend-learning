9f4f5919d3aacd7dbc7e6e8f02090592d1301875
Dao Duc Tam <ddt@gmail.com>
2025-08-15 20:39:52 +0700

    feat: Khởi tạo Project

---
+++ giai-doan-5-nang-cao/lesson-19-Websocket-vs-Long-polling/orders-tracking/orders-tracking/src/main/java/com/tamdao/orders_tracking/OrdersTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.tamdao.orders_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersTrackingApplication.class, args);
+	}
+
+}
---
+++ giai-doan-5-nang-cao/lesson-19-Websocket-vs-Long-polling/orders-tracking/orders-tracking/src/test/java/com/tamdao/orders_tracking/OrdersTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamdao.orders_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
